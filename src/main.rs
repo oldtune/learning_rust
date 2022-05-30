7d78542aa947188f994fa518960e260a93b6a279
QuackQuack <vivaladaman@gmail.com>
2022-05-30 22:16:42 +0700

    try implementing a future and polling using an example from https://rust-lang.github.io/async-book/02_execution/03_wakeups.html#applied-build-a-timer

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use std::future::Future;
+use std::sync::{Arc, Mutex};
+use std::task::{Poll, Waker};
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+pub struct TimerFuture {
+    shared_state: Arc<Mutex<SharedState>>,
+}
+
+impl Future for TimerFuture {
+    type Output = ();
+
+    fn poll(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        let mut shared_state = self.shared_state.lock().unwrap();
+
+        if shared_state.completed {
+            Poll::Ready(())
+        } else {
+            //arrange the wake
+            shared_state.waker = Some(cx.waker().clone());
+            Poll::Pending
+        }
+    }
+}
+
+impl TimerFuture {
+    pub fn new(duration: Duration) -> Self {
+        let shared_state = Arc::new(Mutex::new(SharedState {
+            completed: false,
+            waker: None,
+        }));
+
+        let thread_shared_state = shared_state.clone();
+
+        thread::spawn(move || {
+            thread::sleep(duration);
+            let mut shared_state = thread_shared_state.lock().unwrap();
+            shared_state.completed = true;
+            if let Some(waker) = shared_state.waker.take() {
+                waker.wake();
+            }
+        });
+
+        TimerFuture { shared_state }
+    }
+}
+
+pub struct SharedState {
+    completed: bool,
+    waker: Option<Waker>,
+}
